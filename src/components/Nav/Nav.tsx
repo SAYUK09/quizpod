de2278b985272792411aa86ff3f6465e7115a2a1
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-06-17 23:24:50 +0530

    Login, Logout and User Details in Navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,16 +1,21 @@
 import "./Nav.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
-
+import { useAuth0 } from "@auth0/auth0-react";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 import ExtensionIcon from "@material-ui/icons/Extension";
 
 import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
 import { makeStyles } from "@material-ui/core/styles";
+import { LogoutButton } from "../../auth/Login/LogoutButton";
+import { LoginButton } from "../../auth/Login/LoginButton";
 
 export function Nav() {
   const [value, setValue] = useState("recents");
+  const { user } = useAuth0();
+  console.log(user);
+
   const handleChange = (event: any, newValue: any) => {
     setValue(newValue);
   };
@@ -19,14 +24,14 @@ export function Nav() {
     root: {
       background: "#3b82f6",
       display: "flex",
-      justifyContent: "space-between"
-    }
+      justifyContent: "space-between",
+    },
   });
 
   const navLabel = makeStyles({
     label: {
-      color: "white"
-    }
+      color: "white",
+    },
   });
   const classes = useStyles();
   const labelClass = navLabel();
@@ -58,6 +63,8 @@ export function Nav() {
             icon={<ExtensionIcon />}
           />
         </Link>
+        {user && <p>{user.name} </p>}
+        {user ? <LogoutButton /> : <LoginButton />}
       </BottomNavigation>
     </>
   );
