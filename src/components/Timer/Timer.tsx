1e1cdc5edb9f5b21b3e583647ccfd9a392703578
Sayuri Kamble <68416000+SAYUK09@users.noreply.github.com>
2021-07-06 16:29:42 +0530

    fix : clear options after time ends

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -2,24 +2,29 @@ import "./Timer.css";
 import { useQuiz } from "../../contexts/Quiz-Context";
 import React from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import type { Options } from "../Quiz/Quiz";
 
-export function Timer() {
+export type TimerProp = {
+  setter: React.Dispatch<React.SetStateAction<Options | null>>;
+};
+
+export function Timer({ setter }: TimerProp) {
   const { state, dispatch } = useQuiz();
 
   return (
     <div className="App">
       <CountdownCircleTimer
         isPlaying={
-          state.data.questions.length - state.currentQsNo == 1 ? false : true
+          state.data.questions.length - state.currentQsNo === 1 ? false : true
         }
         size={120}
         duration={15}
         strokeWidth={6}
-        // strokeLinecap={"square"}
         key={state.currentQsNo}
         onComplete={() => {
           dispatch({ type: "NEXT_QUE" });
-
+          console.log(setter);
+          setter(null);
           return [true, 0];
         }}
         colors={[
